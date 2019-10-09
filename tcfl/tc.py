566a7c80ac6212c8a1b87c2baf86c3ee8c4b2cff
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2019-10-09 16:34:50 -0700

    things: clean up and move to new interface schema
    This yanks the old things interface that was embedded in class
    ttbl.test_target and replaces it with the new schema where it is added
    dynamically. It gets rid of a lot of duplicated code.
    
    No backwards compat it maintained, since it is currently unused.
    
    Client side changes to move everything to tcfl/target_ext_things.py
    and registering the exception. Lots of ducplicated code are
    consolidated from tcf and tcfl/ttb_client.py.
    
    In the daemon side, API entry point in ttbd is removed in favour of
    the ttbl.tt_interface method; all entry points in ttbl.test_target are
    moved to ttbl.things.interface, cutting out a lot of duplication.
    
    ttbl/{pc_ykush,usbrl08b,tt_qemu*} drivers ported, but not tested.
    
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>

--- tcfl/tc.py
+++ tcfl/tc.py
@@ -1571,7 +1571,8 @@ class target_c(object):
         assert component == None or isinstance(component, basestring)
         assert isinstance(stream, bool)
         assert isinstance(raw, bool)
-        assert method.upper() in ( "PUT", "GET", "DELETE", "POST" )
+        assert method.upper() in ( "PUT", "GET", "DELETE", "POST" ), \
+            "method must be PUT|GET|DELETE|POST; got %s" % method
 
         if component:
             kwargs['component'] = component
@@ -7126,3 +7127,5 @@ import target_ext_fastboot
 target_c.extension_register(target_ext_fastboot.extension, "fastboot")
 import pos
 target_c.extension_register(pos.extension, "pos")
+import target_ext_things
+target_c.extension_register(target_ext_things.extension, "things")
